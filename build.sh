76b125911c2f5478e0ecb80d84925cce882cff8e
Abhimanyu Ahuja <abhimanyuahuja12@gmail.com>
2025-07-21 22:53:37 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+rustup default stable
+
+# Set Cargo environment variables
 export CARGO_HOME=/tmp/cargo
 export RUSTUP_HOME=/tmp/rustup
+
+# Install Python dependencies
 pip install --upgrade pip
 pip install -r requirements.txt
